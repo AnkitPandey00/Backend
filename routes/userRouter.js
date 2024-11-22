dcf3ddc7d35a4d52d3ef9317303b5e620f5b2cfe
Ankit Kumar Pandey <148574130+AnkitPandey00@users.noreply.github.com>
2024-11-22 23:57:50 +0530

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 import {
   getAllAuthors,
   getMyProfile,
@@ -10,17 +10,8 @@ import { isAuthenticated, isAuthorized } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-const allowedOrigins = ['http://localhost:5173'];
-
-// Middleware to set CORS headers for login route
-const setCorsHeaders = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', allowedOrigins);
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-};
-
 router.post("/register", register);
-router.post("/login", setCorsHeaders, login);
+router.post("/login", login);
 router.get("/logout", isAuthenticated, logout);
 router.get("/myprofile", isAuthenticated, getMyProfile);
 router.get("/authors", getAllAuthors);
