001889191437472faaa1286156a1583258a667e9
Ankit Kumar Pandey <148574130+AnkitPandey00@users.noreply.github.com>
2024-11-22 23:31:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 import app from "./app.js";
 import cloudinary from "cloudinary";
+import cors from "cors";
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
   api_key: process.env.CLOUDINARY_CLIENT_API,
   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 });
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "PUT", "DELETE", "POST"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.get('/',(req,res)=>{
   res.send("Hello World")
