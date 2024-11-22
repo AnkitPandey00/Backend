7cd04780777f207551d8aab6da320f68b9021b8d
Ankit Kumar Pandey <148574130+AnkitPandey00@users.noreply.github.com>
2024-11-22 23:47:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,11 @@
 import app from "./app.js";
 import cloudinary from "cloudinary";
-import cors from "cors";
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
   api_key: process.env.CLOUDINARY_CLIENT_API,
   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 });
-
-const allowedOrigins = ['http://localhost:5173'];
-
-app.use(cors({
-  origin: allowedOrigins,
-  methods: ["GET", "PUT", "DELETE", "POST"],
-  credentials: true,
-}));
-
-// Handle preflight requests
-app.options('*', cors({
-  origin: allowedOrigins,
-  methods: ["GET", "PUT", "DELETE", "POST"],
-  credentials: true,
-}));
-
 
 app.get('/',(req,res)=>{
   res.send("Hello World")
